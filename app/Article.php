be0c9a3fe3310c1c2987ecf52ca9aede1866210d
Vikash Pathak <vikashh.pathak@gmail.com>
2019-11-25 19:18:46 +0530

    Define relationship

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    
+    protected $fillables = [
+        "title", "content"
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
