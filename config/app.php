069b46ac7dcbb11517fdeeb98e1b8ad065e7171a
Vikash Pathak <vikashh.pathak@gmail.com>
2019-12-01 08:29:35 +0530

    Separate graphql files for all types

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        \Nuwave\Lighthouse\Subscriptions\SubscriptionServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
