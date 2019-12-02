906f538942ef990d5b35bd88d5df9691fe19a952
Vikash Pathak <vikashh.pathak@gmail.com>
2019-12-02 16:25:21 +0530

    Create/Use custom scalar.

---
+++ app/GraphQL/Scalars/Senitizer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\GraphQL\Scalars;
+
+use GraphQL\Type\Definition\ScalarType;
+
+/**
+ * Read more about scalars here http://webonyx.github.io/graphql-php/type-system/scalar-types/
+ */
+class Senitizer extends ScalarType
+{
+    /**
+     * Serializes an internal value to include in a response.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function serialize($value)
+    {
+        // Assuming the internal representation of the value is always correct
+        \Log::info("from serialize");
+        return $value;
+
+        // TODO validate if it might be incorrect
+    }
+
+    /**
+     * Parses an externally provided value (query variable) to use as an input
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function parseValue($value)
+    {
+        // TODO implement validation
+        \Log::info("from parseValue");
+        return $value;
+    }
+
+    /**
+     * Parses an externally provided literal value (hardcoded in GraphQL query) to use as an input.
+     *
+     * E.g.
+     * {
+     *   user(email: "user@example.com")
+     * }
+     *
+     * @param  \GraphQL\Language\AST\Node  $valueNode
+     * @param  mixed[]|null  $variables
+     * @return mixed
+     */
+    public function parseLiteral($valueNode, ?array $variables = null)
+    {
+        // TODO implement validation
+        \Log::info("from parseLiteral");
+        return $valueNode->value;
+    }
+}
